5dad64b02377261d89923684d067eceaf0884931
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-04-01 21:39:52 +0600

    Add linked list

---
+++ linkedlist/linkedlist.js
@@ -0,0 +1,178 @@
+import { Node } from "./node.js";
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    let node = new Node(value);
+
+    // store current node
+    let currNode;
+
+    // if list is empty, add elem as head
+    if (this.head == null) this.head = node;
+    else {
+      currNode = this.head;
+
+      // iterate to end of the list
+      while (currNode.next) {
+        // breaks when current node points to null
+        currNode = currNode.next;
+      }
+      // last element points to new node
+      currNode.next = node;
+    }
+
+    this.size++;
+  }
+
+  prepend(value) {
+    let node = new Node(value);
+
+    if (this.head == null) this.head = node;
+    else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  size() {
+    return this.size;
+  }
+
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    let currNode = this.head;
+    while (currNode.next) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  at(index) {
+    let currNode = this.head;
+
+    if (index < 0 || index >= this.size) {
+      return "Enter a valid index";
+    } else {
+      let i = 0;
+
+      while (i != index) {
+        currNode = currNode.next;
+        i++;
+      }
+      return currNode.value;
+    }
+  }
+
+  pop() {
+    let currNode = this.head;
+
+    // breaks at second last node
+    while (currNode.next.next) {
+      currNode = currNode.next;
+    }
+    let last = currNode.next;
+    currNode.next = null;
+    this.size--;
+    return last.value;
+  }
+
+  contains(value) {
+    let currNode = this.head;
+    while (currNode && currNode.value != value) {
+      currNode = currNode.next;
+    }
+    return currNode != null;
+  }
+
+  find(value) {
+    let i = 0;
+    let currNode = this.head;
+
+    while (currNode) {
+      if (currNode.value == value) {
+        return i;
+      }
+      i++;
+      currNode = currNode.next;
+    }
+    return null;
+  }
+
+  toString() {
+    let currNode = this.head;
+    let str = "";
+
+    while (currNode) {
+      str += " ( " + currNode.value + " ) ->";
+      currNode = currNode.next;
+    }
+    str += " null";
+    return str;
+  }
+
+  insertAt(value, index) {
+    if (index < 0 || index > this.size) {
+      console.log("Enter a valid index");
+    } else {
+      let i = 0;
+      let currNode = this.head;
+      let prevNode;
+      while (i < index) {
+        prevNode = currNode;
+        currNode = currNode.next;
+        i++;
+      }
+      let node = new Node(value);
+      prevNode.next = node;
+      node.next = currNode;
+      this.size++;
+    }
+  }
+
+  removeAt(index) {
+    if (index < 0 || index > this.size) {
+      return console.log("Enter a valid index");
+    } else {
+      let currNode = this.head;
+
+      if (index == 0) {
+        this.head = currNode.next;
+      } else {
+        let i = 0;
+        let prevNode;
+
+        while (i < index) {
+          prevNode = currNode;
+          currNode = currNode.next;
+          i++;
+        }
+        prevNode.next = currNode.next;
+      }
+      this.size--;
+    }
+  }
+}
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+list.insertAt("maomao", 2);
+console.log(list.contains("jinshi"));
+list.pop();
+console.log(list.find("maomao"));
+list.removeAt(3);
+console.log(list.toString());
